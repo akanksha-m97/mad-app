36be13843fa6a26a96d3efd2e97406e85a48adf0
hp <akkumaurya327@gmail.com>
2025-04-17 00:12:49 +0530

    finished app

---
+++ app/src/main/java/com/loginpage/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.loginpage.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    setContentView(R.layout.activity_main)
+
+        }
+        }
+
+
