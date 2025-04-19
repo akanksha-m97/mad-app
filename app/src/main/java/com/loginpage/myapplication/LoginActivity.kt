c1d8fcc154b9e62deba92587e3c6a2995b375521
ankita767 <iamankita767@gamil.com>
2025-04-19 20:45:24 +0530

    Commit my side " ankita"

--- app/src/main/java/com/loginpage/myapplication/LoginActivity.kt
+++ app/src/main/java/com/loginpage/myapplication/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.eduQuest
 
 import com.loginpage.myapplication.MainActivity
+import com.loginpage.myapplication.R
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.*
